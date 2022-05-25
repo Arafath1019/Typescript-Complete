f62bfacba9b97787183c8e929da0b002b8b08af4
Yeasin Arafath <arafath.yeasin1019@gmail.com>
2022-05-25 23:34:09 +0600

    TypeScript Final Commit

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,19 +1,15 @@
-import { Sorter } from "./Sorter";
 import { NumberCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
 import { LinkedList } from "./LinkedList";
 
 // Numbers in Array sorting
 const numbersCollection = new NumberCollection([10,3,-5, 0])
-
-const sorter1 = new Sorter(numbersCollection)
-sorter1.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 // String Sorting
 const charactersCollection = new CharactersCollection('Xaayb');
-const sorter2 = new Sorter(charactersCollection);
-sorter2.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 // LinkedList Sorting
@@ -23,7 +19,6 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
 
-const sorter3 = new Sorter(linkedList);
-sorter3.sort();
+linkedList.sort();
 linkedList.print();
 
